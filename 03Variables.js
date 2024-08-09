ca86e5ae6248b04d95d32d1237fa92d3255e76ab
aman <amankatiyar1572@gmail.com>
2024-08-09 06:49:58 +0000

    new files created

---
+++ 03Variables.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+console.log("aman");
+console.log(3+3);
+
+let name = "aman"
+let age = 21
+let isLoggedIn = false
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = false/true
+// null => stand alone value zero nhi h khali h
+//undedined=>
+    // symbol = unique
+
+// object
+console.log(typeof "aman");
+
